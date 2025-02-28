6d3df4140d493a8e7f99d13a03831db24d6603e4
Luka Barth <luka.costa@db1.com.br>
2025-02-28 15:42:37 -0300

    refactor: add 'Categoria' attribute in base class

--- src/Produtos/Produtos.php
+++ src/Produtos/Produtos.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace App\Produtos;
 
+use App\Produtos\Categoria;
+
 abstract class Produtos {
     protected int $codigo;
     protected string $nome;
     protected float $precoUnitario;
     protected string $unidadeMedida;
+    protected Categoria $categoria;
 
-    public function __construct(int $codigo, string $nome, float $precoUnitario, string $unidadeMedida) {
+    public function __construct(int $codigo, string $nome, float $precoUnitario, string $unidadeMedida, Categoria $categoria) {
         $this->codigo = $codigo;
         $this->nome = $nome;
         $this->precoUnitario = $precoUnitario;
         $this->unidadeMedida = $unidadeMedida;
+        $this->categoria = $categoria;
     }
 
     public function getCodigo(): int {
@@ -29,11 +33,16 @@ abstract class Produtos {
         return $this->precoUnitario;
     }
 
+    public function getUnidadeMedida(): string {
+        return $this->unidadeMedida;
+    }
+
     public function detalhesProduto(): string {
-        return "Código: $this->codigo" . PHP_EOL .
-                "Nome: $this->nome" . PHP_EOL .
-                "Preço: $this->precoUnitario" . PHP_EOL .
-                "Unidade de medida: $this->unidadeMedida" . PHP_EOL;
+        return "Código: {$this->getCodigo()}" . PHP_EOL .
+                "Nome: {$this->getNome()}" . PHP_EOL .
+                "Preço: {$this->getPrecoUnitario()}" . PHP_EOL .
+                "Unidade de medida: {$this->getUnidadeMedida()}" . PHP_EOL . 
+                "Categoria do produto: {$this->categoria->getNome()}" . PHP_EOL;
     }
 
     abstract public function exibirDetalhes(): void;
